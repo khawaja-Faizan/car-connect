aee7cff9c494bd1c65e4d02831f8637eddd7c814
Khawaja Faizan <khawajafaizan040@gmail.com>
2023-09-27 21:37:40 +0500

    Added the basic structure for the app including a bit of functionality in navbar

---
+++ src/system/routes.js
@@ -0,0 +1,22 @@
+export const routes = [
+  {
+    name: "Home",
+    path: "#home",
+  },
+  {
+    name: "Cars",
+    path: "#cars",
+  },
+  {
+    name: "About us",
+    path: "#about_us",
+  },
+  {
+    name: "Why us",
+    path: "#why_us",
+  },
+  {
+    name: "Contact",
+    path: "#contact",
+  },
+];
---
+++ src/views/about/about.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about" id="about_us">
+      <div>About</div>
+    </section>
+  );
+};
+
+export default About;
