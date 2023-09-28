061b170db09fcb15d1c81b15904295116b051a5e
Khawaja Faizan <khawajafaizan040@gmail.com>
2023-09-29 01:11:10 +0500

    Added a carousel in the home view

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import carOne from "../../assets/images/car01.png";
+import carTwo from "../../assets/images/car02.png";
+import carThree from "../../assets/images/car03.png";
+import { Carousel } from "react-bootstrap";
 
 const Home = () => {
+  const slides = [
+    {
+      image: carOne,
+      header: "Luxury cars starting from 30$ a day",
+      smallHeader: "Plan your trip now",
+    },
+    {
+      image: carTwo,
+      header: "Get 15% off on your 1st rental",
+      smallHeader: "Treat yourself",
+    },
+    {
+      image: carThree,
+      header: "Luxury cars starting from 30$ a day",
+      smallHeader: "Plan your trip now",
+    },
+  ];
+
   return (
     <section className="home" id="home">
-      <div>Home</div>
+      <Carousel touch={true} className="home-carousel">
+        {slides.map((slide, index) => (
+          <Carousel.Item>
+            <h3>{slide.header}</h3>
+            <p>{slide.smallHeader}</p>
+            <img src={slide.image} alt={`car-${index}`} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <div className="bottom-arrow"></div>
     </section>
   );
 };
